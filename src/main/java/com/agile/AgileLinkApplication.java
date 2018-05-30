fce5ba8dbed1cea11a4e19ef4807f5a217575976
Ray Rui <Ray.Rui@lombardrisk.com>
2018-05-30 15:20:50 +0800

    start project.

---
+++ src/main/java/com/agile/AgileLinkApplication.java
@@ -0,0 +1,12 @@
+package com.agile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileLinkApplication.class, args);
+	}
+}
